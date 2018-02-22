d8a42cbb334ab8fdd1efcee47148a01b1c83852a
SCHOOLSPEAK33\SSDev <abhinavm@schoolspeak.com>
2018-02-22 14:38:28 +0530

    Added the Member and Members Collection models to the Standard Data Set project

---
+++ CB.IntegrationService/CB.IntegrationService.StandardDataSet/Models/MembersCollection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CB.IntegrationService.StandardDataSet.Models
+{
+    public class MembersCollection
+    {
+        /// <summary>
+        /// Gets or sets the members.
+        /// </summary>
+        /// <value>
+        /// The members.
+        /// </value>
+        public List<Member> Members { get; set; }
+    }
+}
